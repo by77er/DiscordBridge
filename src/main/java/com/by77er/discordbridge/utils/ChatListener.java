b5934e1e2ac26646c6d5b462080af88a9541e79e
by77er <39721110+by77er@users.noreply.github.com>
2019-08-04 15:16:52 -0500

    Escaping

--- src/main/java/com/by77er/discordbridge/utils/ChatListener.java
+++ src/main/java/com/by77er/discordbridge/utils/ChatListener.java
@@ -11,7 +11,6 @@ import org.bukkit.event.entity.PlayerDeathEvent;
 import org.bukkit.ChatColor;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.player.AsyncPlayerChatEvent;
-import org.bukkit.event.player.PlayerAdvancementDoneEvent;
 import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.event.player.PlayerQuitEvent;
 
@@ -69,7 +68,7 @@ public class ChatListener implements Listener {
                 .POST(BodyPublishers.ofString(""
                 + "{"
                 + " \"username\": \"" + username + "\","
-                + " \"content\": \"" + message + "\""
+                + " \"content\": \"" + message.replace("\\", "\\\\").replace("\"", "\\\"") + "\""
                 + "}"))
                 .build();
             // send request
